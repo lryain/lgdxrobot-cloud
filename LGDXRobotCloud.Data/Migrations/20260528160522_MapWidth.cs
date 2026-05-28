602d52170d92961566aceeb83ae4d8db020572d4
Kai Tung Yu <sam@bristolgram.uk>
2026-05-28 17:06:01 +0100

    update migration

---
+++ LGDXRobotCloud.Data/Migrations/20260528160522_MapWidth.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LGDXRobotCloud.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MapWidth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MapHeight",
+                table: "Navigation.Realms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MapWidth",
+                table: "Navigation.Realms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Navigation.Realms",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "MapHeight", "MapWidth" },
+                values: new object[] { 0, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MapHeight",
+                table: "Navigation.Realms");
+
+            migrationBuilder.DropColumn(
+                name: "MapWidth",
+                table: "Navigation.Realms");
+        }
+    }
+}
